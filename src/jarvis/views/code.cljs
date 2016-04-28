58121935e16b59c4cef26764b4d459257d25e426
scoiatael <czapl.luk+git@gmail.com>
2016-04-28 12:52:38 +0000

    Removed ability to paste nodes to defs
    Also switched to hash-map for passing additional options

--- src/jarvis/views/code.cljs
+++ src/jarvis/views/code.cljs
@@ -9,30 +9,33 @@
 
 (defonce ^:private *introspect* (atom false))
 
-(defn- code [id pasting? item index]
-  (let [item-to-show (if @*introspect* (->> item walk/normalize sc/parse) item)]
-    [rc/border
-     :border (str "1px dashed " "transparent")
-     :child [r/render
-             {:on-click #(dispatch [:node-clicked %1 %2])
-              :path []
-              :paster pasting?
-              :id id
-              :on-hover #(dispatch [:node-hover %1 %2])}
-             item-to-show]]))
+(defn- code [h item index]
+  (let [id (:id h)
+        pasting? (:pasting h)]
+    (let [item-to-show (if @*introspect* (->> item walk/normalize sc/parse) item)]
+      [rc/border
+       :border (str "1px dashed " "transparent")
+       :child [r/render
+               {:on-click #(dispatch [:node-clicked %1 %2])
+                :path []
+                :paster pasting?
+                :id id
+                :on-hover #(dispatch [:node-hover %1 %2])}
+               item-to-show]])))
 
-(defn- code-list [id pasting? codes]
+(defn- code-list [h codes]
   [v-box
    :align :start
    :style {:max-width "100%"}
-   :children (map-indexed (fn [index item] [code id pasting? item index]) codes)])
+   :children (map-indexed (fn [index item] [code h item index]) codes)])
 
 (defn- code-boxes [pasting? defs scratch]
-  [v-box
-   :gap "1em"
-   :children [[code-list :defs pasting? defs]
-              [rc/line]
-              [code-list :scratch pasting? scratch]]])
+  (let [h {:pasting pasting?}]
+    [v-box
+     :gap "1em"
+     :children [[code-list (assoc h :id :defs :pasting false) defs]
+                [rc/line]
+                [code-list (assoc h :id :scratch) scratch]]]))
 
 (defn render []
   (let [pasting? (subscribe [:pasting?])
