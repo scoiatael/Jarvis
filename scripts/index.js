01fd7917d13b8d8740d1fbf24be82f6b50b90032
L E <L E>
2016-07-31 23:50:12 +0200

    Init React + Bootstrap page

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const DownloadLink = () => {
+  return (
+    <a href="#" className="btn btn-lg btn-default">Download</a>
+  );
+}
+
+const Page = () => {
+  return (
+    <div className="inner cover">
+      <h1 className="cover-heading">Visual Programming Editor - reimagined.</h1>
+      <p className="lead">Jarvis is a simple visual Clojure editor.</p>
+      <p className="lead">
+        < DownloadLink />
+      </p>
+    </div>
+    );
+}
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('content')
+);
